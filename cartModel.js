7aa32960395d95b489e0878823d987c31e9f080d
sanju29siya-ssr <sanju29siya@gmail.com>
2025-11-06 00:47:53 +0530

    Delete cartModel.js

--- cartModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const cartSchema = new mongoose.Schema({
-  userId: String,
-  productId: String,
-  quantity: { type: Number, default: 1 }
-});
-
-module.exports = mongoose.model('Cart', cartSchema);
